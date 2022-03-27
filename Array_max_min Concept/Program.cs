7d67dda8948a6c13d93cc4e32441da7b35d6b538
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-28 04:11:49 +0530

    Add project files.

---
+++ Array_max_min Concept/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array_max_min_Concept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] ar = { 1, 12, 10, 115, 10, 12, 1, 10, 1000001 };
+
+            int max = ar[0];
+            int Min = ar[0];
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if(max<ar[i])
+                {
+                    max = ar[i];
+                }
+                if (Min > ar[i])
+                {
+                    Min = ar[i];
+                }
+
+            }
+            Console.WriteLine("maximum is:"+max);
+            Console.WriteLine("Minimum is:" + Min);
+
+        }
+    }
+}
